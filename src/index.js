d182f7cf08a284d3ac6672d76207d8322bc57248
momotsow <kgomotsomkansi@gmail.com>
2022-12-09 19:04:52 +0200

    Display components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import BookstoreContainer from './pages/AppContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <BookstoreContainer />
+    </Router>
   </React.StrictMode>,
 );
 
