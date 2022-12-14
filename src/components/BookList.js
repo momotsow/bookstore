9468e50ee1d2785040468fb5be3cdeb85ed8a3c3
momotsow <kgomotsomkansi@gmail.com>
2022-12-14 10:52:03 +0200

    Fix Linter issues

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Book from './BookItem';
 import { v4 as uuidv4 } from 'uuid';
 import { useSelector } from 'react-redux';
+import Book from './BookItem';
 import Form from './Form';
 
 function BookList() {
