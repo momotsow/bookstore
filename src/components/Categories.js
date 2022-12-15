c728261a03cab2800334d569316e7c92d5e4326d
momotsow <kgomotsomkansi@gmail.com>
2022-12-15 13:17:28 +0200

    Use dispatch for categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 function Categories() {
   const status = useSelector((state) => state.categoriesReducers);
+  const dispatch = useDispatch();
   return (
     <div>
       <button type="button" onClick={() => { dispatch(checkStatus()); }}>Check Status</button>
