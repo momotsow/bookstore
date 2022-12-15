be70d601834e4fc134f4c38369a12c50448a3e0d
momotsow <kgomotsomkansi@gmail.com>
2022-12-15 18:05:15 +0200

    Remove unused assignments

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
 import Book from './BookItem';
 import Form from './Form';
